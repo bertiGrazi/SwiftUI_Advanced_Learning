92f9b633e1136b9688ac0b9dd60d0fe1c3c73437
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-24 15:09:24 -0300

    first step of integration

--- Airbnb/Airbnb/Services/APIService.swift
+++ Airbnb/Airbnb/Services/APIService.swift
@@ -13,4 +13,15 @@ final class APIService {
     struct Constants {
         static let apiUrl = URL(string: "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/airbnb-listings/records?limit=100&refine=city%3A%22New%20York%22&refine=room_type%3A%22Entire%20home%2Fapt%22")
     }
+    
+    func getListing(completion: @escaping(Result<[AirbnbListings], Error>) -> Void) {
+        guard let url = Constants.apiUrl else {
+            return
+        }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            
+        }
+        task.resume()
+    }
 }
