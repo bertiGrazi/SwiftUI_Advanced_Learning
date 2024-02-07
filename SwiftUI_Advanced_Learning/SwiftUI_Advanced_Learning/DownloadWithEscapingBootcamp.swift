91ccf23b648b27d573f7e8de6e67bfe3a3a5be72
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-02-07 10:10:48 -0300

    get post data in a view

--- SwiftUI_Advanced_Learning/SwiftUI_Advanced_Learning/DownloadWithEscapingBootcamp.swift
+++ SwiftUI_Advanced_Learning/SwiftUI_Advanced_Learning/DownloadWithEscapingBootcamp.swift
@@ -7,17 +7,27 @@
 
 /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 /// https://jsonplaceholder.typicode.com/
+/// https://app.quicktype.io/
 
 import SwiftUI
 
+struct PostModel: Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
 class DownloadWithEscapingViewModel: ObservableObject {
+    
+    @Published var posts: [PostModel] = []
     
     init() {
         getPost()
     }
     
     func getPost() {
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else { return }
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/2") else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data else {
@@ -46,15 +56,32 @@ class DownloadWithEscapingViewModel: ObservableObject {
             
             let jsonString = String(data: data, encoding: .utf8)
             print(jsonString)
+            
+            guard let newPost = try? JSONDecoder().decode(PostModel.self, from: data) else { return }
+            DispatchQueue.main.async { [weak self] in
+                self?.posts.append(newPost)
+            }
+            
         }.resume()
     }
 }
 
 struct DownloadWithEscapingBootcamp: View {
-    @StateObject var vireModel = DownloadWithEscapingViewModel()
+    @StateObject var viewModel = DownloadWithEscapingViewModel()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(viewModel.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    
+                    Text(post.body)
+                        .foregroundStyle(Color.gray)
+                }
+                
+            }
+        }
     }
 }
 
